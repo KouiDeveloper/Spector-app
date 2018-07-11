fe9494dcd93e146dcd2f7155c25124a998f0fe77
KouiDeveloper <kouiprogramer@gmail.com>
2018-07-11 12:05:45 +0700

    update 11.7.2018

--- src/app/a006-subtract-products/a006-subtract-products.component.ts
+++ src/app/a006-subtract-products/a006-subtract-products.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-a006-subtract-products',
   templateUrl: './a006-subtract-products.component.html',
-  styleUrls: ['./a006-subtract-products.component.css','../a005-add-products/a005-add-products.component.css']
+  styleUrls: ['./a006-subtract-products.component.css']
 })
 export class A006SubtractProductsComponent implements OnInit {
 
