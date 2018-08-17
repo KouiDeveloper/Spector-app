8460957342030c852caca311fb17c56db1dca1ab
KouiDeveloper <kouiprogramer@gmail.com>
2018-08-17 15:41:19 +0700

    update 17.8.2018 3:41 PM

--- src/app/c006-make-order/c006-make-order.component.ts
+++ src/app/c006-make-order/c006-make-order.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-c006-make-order',
   templateUrl: './c006-make-order.component.html',
-  styleUrls: ['./c006-make-order.component.css','./c006-customStyle.css']
+  styleUrls: ['./c006-make-order.component.css','../main-css/scroll.css']
 })
 export class C006MakeOrderComponent implements OnInit {
 
